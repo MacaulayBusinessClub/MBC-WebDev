1b5db0fc38da8cddff3794216a7ba28c5070ab28
Ramanpreet Chand <rchand595@gmail.com>
2017-03-05 02:36:52 -0500

    added mobile nav and new social icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,7 @@
-// $(document).ready(function() {
-// 	$('button').click(function() {
-// 		$('.vanish').fadeOut('slow');
-// 	});
-// });
-
-/* Set the width of the side navigation to 250px */
 function openNav() {
 	document.getElementById("sideNavigation").style.width = "20em";
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
 	document.getElementById("sideNavigation").style.width = "0";
 }
