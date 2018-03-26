301d060dceb84c93f0420ea973f9a0b37a5286fc
Boubacar Diallo <boubacar.diallo1@macaulay.cuny.edu>
2018-03-26 06:05:16 -0400

    new json

---
+++ assets/js/members.js
@@ -0,0 +1,68 @@
+$(document ).ready(function() {
+    var members = 0;
+    var requestURL = 'https://macaulaybusinessclub.github.io/pcmembers.json';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        loadmembers(request.response);
+    }
+});
+
+function loadmembers(json){
+    members = json["Roster"];
+    for(var i = 0; i < members.length;i++){
+        member = members[i];
+        var name = member["name"];
+        var image = member.imageUrl;
+        var linkedin = member.linkedin;
+        var position = member.position;
+        
+        var card = "<div class='col-md-3 col-xs-6'>" + "<div class='portrait'>" + 
+        "<img class='card-img-top' src='" + image + "' alt='" + name + "' width='100%' height='100%'>" + 
+          "<div class='card-block'>" + 
+              "<h4 class='card-title'>" + name + "</h4>" +
+              "<p class='card-text'>" + position + "</p>" + 
+              "<a class='card-social' href='" + linkedin + "'><img src='assets/icons/LinkedIn_48x48.png' width='36' height='36'></a>" + 
+          "</div> </div> </div>"
+
+          $("#memberdisplay").append(card);
+    }
+}
+
+    /*
+    for(member in json["pcmembers"]){
+        var name = member["name"];
+        var image = member.imageUrl;
+        var linkedin = member.linkedin;
+        var position = member.position;
+        
+        var card = "<div class='col-md-3 col-xs-6'>" + "<div class='portrait'>" + 
+        "<img class='card-img-top' src='" + image + "' alt='" + name + "' width='100%' height='100%'>" + 
+          "<div class='card-block'>" + 
+              "<h4 class='card-title'>" + name + "</h4>" +
+              "<p class='card-text'>" + position + "</p>" + 
+              "<a class='card-social' href='" + linkedin + "'><img src='assets/icons/LinkedIn_48x48.png' width='36' height='36'></a>" + 
+          "</div> </div> </div>"
+
+          $("#memberdisplay").append(card);
+    }
+    
+
+    /*
+    $.getJSON("../../pcmembers.json", function(data){
+        $.each( data, function( key, val ) {
+            alert("HUh");
+            var member = "<div class='col-md-3 col-xs-6'>" + "<div class='portrait'>" + 
+              "<img class='card-img-top' src='" + val.imageUrl + "' alt='" + val.name + "' width='100%' height='100%'>" + 
+                "<div class='card-block'>" + 
+                    "<h4 class='card-title'>" + val.name + "</h4>" +
+                    "<p class='card-text'>Executive President</p>" + 
+                    "<a class='card-social' href='https://www.linkedin.com/in/othillawlod/'><img src='assets/icons/LinkedIn_48x48.png' width='36' height='36'></a>" + 
+                "</div> </div> </div>"
+        });
+    });
+    $("#memberdisplay").append(member);
+    */
+
