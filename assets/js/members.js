37e5e1a196a31fbf5eebfac76d5e999d4b65c720
Boubacar Diallo <boubacar.diallo1@macaulay.cuny.edu>
2018-04-19 16:10:10 -0400

    forced same size images for members

--- assets/js/members.js
+++ assets/js/members.js
@@ -1,5 +1,4 @@
-$(document ).ready(function() {
-    var members = 0;
+$(document).ready(function() {
     var requestURL = 'https://macaulaybusinessclub.github.io/pcmembers.json';
     var request = new XMLHttpRequest();
     request.open('GET', requestURL);
@@ -11,43 +10,34 @@ $(document ).ready(function() {
 });
 
 function loadmembers(json){
-    members = json["Roster"];
+    var members = json["Roster"];
     for(var i = 0; i < members.length;i++){
-        member = members[i];
+        var card = "";
+        if(i % 4 == 0){ 
+         $("#memberdisplay").append("<div class='row portrait-row'>");
+        }
+        var member = members[i];
         var name = member["name"];
-        var image = member.imageUrl;
+        var image = member.imageurl;
         var linkedin = member.linkedin;
         var position = member.position;
-        
-        var card = "<div class='col-md-3 col-xs-6'>" + "<div class='portrait'>" + 
-        "<img class='card-img-top' src='" + image + "' alt='" + name + "' width='100%' height='100%'>" + 
-          "<div class='card-block'>" + 
-              "<h4 class='card-title'>" + name + "</h4>" +
-              "<p class='card-text'>" + position + "</p>" + 
-              "<a class='card-social' href='" + linkedin + "'><img src='assets/icons/LinkedIn_48x48.png' width='36' height='36'></a>" + 
-          "</div> </div> </div>"
+
+        card = "<div class='col-md-3 col-xs-6'>" + 
+                        "<div class = 'member'>" +
+                        "<div class='portrait'>" + 
+                            "<img class='card-img-top member-image' src='" + image + "' alt='" + name + "' width='100%' height='100%'>" + 
+                           "<div class='card-block'>" + 
+                                "<h4 class='card-title'>" + name + "</h4>" +
+                                "<p class='card-text'>" + position + "</p>" + 
+                                "<a class='card-social' href='" + linkedin + "'><img src='assets/icons/LinkedIn_48x48.png' width='36' height='36'></a>" + 
+                    " </div> </div> </div> </div>"
 
           $("#memberdisplay").append(card);
+        if(i % 4 == 3){    
+        $("#memberdisplay").append("</div");
+        }
     }
 }
-
-    /*
-    for(member in json["pcmembers"]){
-        var name = member["name"];
-        var image = member.imageUrl;
-        var linkedin = member.linkedin;
-        var position = member.position;
-        
-        var card = "<div class='col-md-3 col-xs-6'>" + "<div class='portrait'>" + 
-        "<img class='card-img-top' src='" + image + "' alt='" + name + "' width='100%' height='100%'>" + 
-          "<div class='card-block'>" + 
-              "<h4 class='card-title'>" + name + "</h4>" +
-              "<p class='card-text'>" + position + "</p>" + 
-              "<a class='card-social' href='" + linkedin + "'><img src='assets/icons/LinkedIn_48x48.png' width='36' height='36'></a>" + 
-          "</div> </div> </div>"
-
-          $("#memberdisplay").append(card);
-    }
     
 
     /*
